430e1da46da2e7df44b8839bb58feaa67054c2dc
Jonathan Schenker <jonathan.f.schenker@vanderbilt.edu>
2015-10-08 12:03:15 -0500

    Added base html file

--- GradingCalc/index.js
+++ GradingCalc/index.js
@@ -1,3 +1,22 @@
 /**
  * Created by jonathanschenker on 10/8/15.
  */
+var express = require('express'),
+    bodyParser = require('body-parser');
+
+var app = express();
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+
+
+
+
+
+
+
+var server = app.listen(8080, function () {
+    console.log('Example app listening on ' + server.address().port);
+});
