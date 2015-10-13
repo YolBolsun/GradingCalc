af45e113ea8ce04a95f7ee2092fd20af26ff190d
unknown <jonathan.a.clinton@vanderbilt.edu>
2015-10-13 11:27:59 -0500

    added empty test files

---
+++ GradingCalc/test/test.js
@@ -0,0 +1,27 @@
+var assert = require('assert');
+var should = require('should');
+var fs = require('fs');
+var _ = require('lodash');
+var chai = require('chai');
+var expect = chai.expect;
+var sinonChai = require('sinon-chai');
+require('mocha-sinon');
+chai.use(sinonChai);
+
+describe('lib/sync', function(){
+    describe('#add', function() {
+
+        before(function(){
+
+        });
+
+        after(function() {
+
+        });
+
+        it('should prompt for a filename if one is not given', function() {
+
+        });
+
+    });
+  });
